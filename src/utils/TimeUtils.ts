ba9fa0fc5229885c0d60e5800d2f6ceb420f351c
MrWakeCZ <joseff.kral@gmail.com>
2021-07-09 02:14:24 +0200

    Oprava počítání času, když minuty jsou pod 10m

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -1,6 +1,6 @@
 
 export const convertMsToTime = (millis: number): string => {
-    let minutes = Math.floor(millis / 60000);
-    let seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + seconds;
+    let minutes: number = Math.floor(millis / 60000);
+    let seconds: number = Number(((millis % 60000) / 1000).toFixed(0));
+    return minutes + ":" + (seconds < 10 ? '0' + seconds : seconds);
 }
