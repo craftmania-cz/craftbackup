28376b43db09b70afe0ef3b9d9791e7e1da07e01
MrWakeCZ <joseff.kral@gmail.com>
2021-08-11 23:47:03 +0200

    Dynamické načítání tasků dle poskytnutých configů

--- src/Application.ts
+++ src/Application.ts
@@ -1,6 +1,7 @@
 import { Logger } from "./utils/Logger";
 import BackupManager from "./managers/BackupManager";
 import FTPCleanManager from "./managers/FTPCleanManager";
+import * as fs from "fs";
 
 const log = Logger('app:application');
 
@@ -11,8 +12,23 @@ export default class Application {
 
     public async start() {
         log.info("Startup main app thread.");
-        await this.backupManager.loadBackups();
-        await this.ftpCleanUpManager.loadFtpCleanUpTasks();
+
+        if (!fs.existsSync("./backups")) {
+            log.error("Folder for backups /backup not found.");
+            process.exit(1);
+        }
+
+        if (fs.existsSync("./config/backup-mysql.json")) {
+            log.info("Backup file for mysql backups found.");
+            await this.backupManager.loadBackups();
+        }
+
+        if (fs.existsSync("./config/ftp-clean-tasks.json")) {
+            log.info("FTP clean tasks file found.");
+            await this.ftpCleanUpManager.loadFtpCleanUpTasks();
+        }
+
+        log.info("Application startup completed.");
 
     }
 
